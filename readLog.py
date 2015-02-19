d79ac4efc6aad4d39470a36b5873622eceab9177
KarlJohan <karl-johan.hultin@labgruppen.com>
2015-02-19 22:42:16 +0100

    readLog improvements and asserts

--- readLog.py
+++ readLog.py
@@ -11,6 +11,9 @@ def test_bitStruct(bitStruct):
     assert(summa % 32 == 0)
     assert(summa / 32 == 112)
 
+    for fmt, _, l in bitStruct:
+        assert(int(fmt.split(':')[1]) == l)
+
 
 def splitBitStructIn32bits(bitStruct):
     acc = 0
@@ -32,15 +35,27 @@ def entryToFmtString(entry):
     return rString
 
 
-def getLogEntryFromDevice():
+def getLogEntryFromDevice(le_index=120):
+    
+    assert(0 < le_index <= 120)  # First index is 1 (not 0)
+    
     # amp = LakePP("169.254.166.60", "d3000015:6b44ed00")
     amp = LakePP("127.0.0.1", "BEE7ACE5:55E78008")
 
-    logEnteryBaseAddr = 0x70002
-    logEnteryAddrRange = range(logEnteryBaseAddr, (logEnteryBaseAddr + 112))
+    le_NumberOfEnts  = 120
+    le_Length        = 112
+    le_BaseAddr      = 0x70002
+    le_ToRead        = le_BaseAddr + le_Length * (le_index-1)  # Oldest entry is in the beginning, we want the newest/last
+    le_AddrRange     = range(le_ToRead, (le_ToRead + le_Length))
+    assert(le_AddrRange[-1] == 0x073481)
 
-    return [amp.read_param(a)[0] for a in logEnteryAddrRange]
+    return [amp.read_param(a)[0] for a in le_AddrRange]
 
+
+def test_ValidLogDict(logDict):
+    assert(logDict['ucTag0'] == ord('L'))
+    assert(logDict['ucTag1'] == ord('O'))
+    assert(logDict['ucTag2'] == ord('G'))
 
 if __name__ == '__main__':
 
@@ -50,7 +65,6 @@ if __name__ == '__main__':
     logDict = OrderedDict()
     logEntry = map(str, getLogEntryFromDevice())
     assert(len(logEntry) == 112)
-    print logEntry
 
     for logE in logEntry:
         rawBits = BitArray('int:32=' + logE)
@@ -59,7 +73,17 @@ if __name__ == '__main__':
         for (k, v) in zip(ent, b):
             logDict[k[1]] = v
 
-    print logDict
-    print logDict['global.accWatisar']
-    print logDict['ChLogEntry_ChB.usPtgZ0']
-    print logDict['ChLogEntry_ChB.usPtgZ1']
+    test_ValidLogDict(logDict)
+    # print logDict
+    print 'ucChksum: ', logDict['ucChksum']
+    print 'uiTimestamp: ', logDict['uiTimestamp']
+    print 'global.accWatisar: ', logDict['global.accWatisar']
+    print 'ChLogEntry_ChA.usPtgZ0: ', logDict['ChLogEntry_ChA.usPtgZ0']
+    print 'ChLogEntry_ChA.usPtgZ1: ', logDict['ChLogEntry_ChA.usPtgZ1']
+    print 'ChLogEntry_ChB.usPtgZ0: ', logDict['ChLogEntry_ChB.usPtgZ0']
+    print 'ChLogEntry_ChB.usPtgZ1: ', logDict['ChLogEntry_ChB.usPtgZ1']
+    print 'ChLogEntry_ChC.usPtgZ0: ', logDict['ChLogEntry_ChC.usPtgZ0']
+    print 'ChLogEntry_ChC.usPtgZ1: ', logDict['ChLogEntry_ChC.usPtgZ1']
+    print 'ChLogEntry_ChD.usPtgZ0: ', logDict['ChLogEntry_ChD.usPtgZ0']
+    print 'ChLogEntry_ChD.usPtgZ1: ', logDict['ChLogEntry_ChD.usPtgZ1']
+
